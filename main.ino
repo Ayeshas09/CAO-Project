c7030c4addd9ca877b78a0586876cfefff6303e2
Ayesha <asurryya@gmail.com>
2022-05-16 16:22:13 +0100

    code refactoring

--- main.ino
+++ main.ino
@@ -1,14 +1,12 @@
-#include <WiFi.h> 
-
+#include <WiFi.h>
 #include <Wire.h>
-
 #include "DHT.h"
 
 #define DHTTYPE DHT11 // DHT 11
 
-uint8_t DHTPin = 4; 
+uint8_t DHTPin = 4;
 
-DHT dht(DHTPin, DHTTYPE); 
+DHT dht(DHTPin, DHTTYPE);
 
 float Temperature;
 
@@ -16,9 +14,9 @@ float Humidity;
 
 void send_event(const char *event);
 
-const char* ssid = "AndroidAPAA86"; 
+const char *ssid = "AndroidAPAA86";
 
-const char* password = "wiuw1034";
+const char *password = "wiuw1034";
 
 const char *host = "maker.ifttt.com";
 
@@ -28,142 +26,139 @@ WiFiServer server(80);
 
 String header;
 
-void setup() {
+void setup()
+{
+  Serial.begin(115200);
 
-Serial.begin(115200);
+  pinMode(DHTPin, INPUT);
 
-pinMode(DHTPin, INPUT);
+  dht.begin();
 
-dht.begin();
+  Serial.print("Connecting to Wifi Network");
 
-Serial.print("Connecting to Wifi Network");
+  Serial.println(ssid);
 
-Serial.println(ssid);
+  WiFi.begin(ssid, password);
 
-WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
 
-while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
 
-delay(500);
+    Serial.print(".");
+  }
 
-Serial.print(".");
+  Serial.println("");
 
-}
+  Serial.println("Successfully connected to WiFi.");
 
-Serial.println("");
+  Serial.println("IP address of ESP32 is : ");
 
-Serial.println("Successfully connected to WiFi.");
+  Serial.println(WiFi.localIP());
 
-Serial.println("IP address of ESP32 is : ");
+  server.begin();
 
-Serial.println(WiFi.localIP());
-
-server.begin();
-
-Serial.println("Server started");
-
+  Serial.println("Server started");
 }
 
 void loop()
 
 {
 
-Temperature = dht.readTemperature(); 
+  Temperature = dht.readTemperature();
 
-Humidity = dht.readHumidity(); 
+  Humidity = dht.readHumidity();
 
-WiFiClient client = server.available();
+  WiFiClient client = server.available();
 
-if (client) 
+  if (client)
 
-{ 
+  {
 
-Serial.println("Web Client connected ");
+    Serial.println("Web Client connected ");
 
-String request = client.readStringUntil('\r'); 
+    String request = client.readStringUntil('\r');
 
-client.println("HTTP/1.1 200 OK");
+    client.println("HTTP/1.1 200 OK");
 
-client.println("Content-Type: text/html");
+    client.println("Content-Type: text/html");
 
-client.println("Connection: close");  // the connection will be closed after completion of the response
+    client.println("Connection: close"); // the connection will be closed after completion of the response
 
-client.println("Refresh: 10");  // update the page after 10 sec
+    client.println("Refresh: 10"); // update the page after 10 sec
 
-client.println();
+    client.println();
 
-client.println("<!DOCTYPE HTML>");
+    client.println("<!DOCTYPE HTML>");
 
-client.println("<html>");
+    client.println("<html>");
 
-client.println("<style>html { font-family: Cairo; display: block; margin: 0px auto; text-align: center;color: #333333; background-color: ##f3ffee;}");
+    client.println("<style>html { font-family: Cairo; display: block; margin: 0px auto; text-align: center;color: #333333; background-color: ##f3ffee;}");
 
-client.println("body{margin-top: 50px;}");
+    client.println("body{margin-top: 50px;}");
 
-client.println("h1 {margin: 50px auto 30px; font-size: 50px;  text-align: center;}");
+    client.println("h1 {margin: 50px auto 30px; font-size: 50px;  text-align: center;}");
 
-client.println(".side_adjust{display: inline-block;vertical-align: middle;position: relative;}");
+    client.println(".side_adjust{display: inline-block;vertical-align: middle;position: relative;}");
 
-client.println(".text1{font-weight: 180; padding-left: 15px; font-size: 50px; width: 170px; text-align: left; color: #3498db;}");
+    client.println(".text1{font-weight: 180; padding-left: 15px; font-size: 50px; width: 170px; text-align: left; color: #3498db;}");
 
-client.println(".data1{font-weight: 180; padding-left: 70px; font-size: 50px;color: #3498db;}");
+    client.println(".data1{font-weight: 180; padding-left: 70px; font-size: 50px;color: #3498db;}");
 
-client.println(".text2{font-weight: 180;padding-left: 15px; font-size: 50px; width: 170px; text-align: left; color: #ff6600;}");
+    client.println(".text2{font-weight: 180;padding-left: 15px; font-size: 50px; width: 170px; text-align: left; color: #ff6600;}");
 
-client.println(".data2{font-weight: 180; padding-left: 120px; font-size: 50px;color: #ff6600;}");
+    client.println(".data2{font-weight: 180; padding-left: 120px; font-size: 50px;color: #ff6600;}");
 
-client.println(".data{padding: 10px;}");
+    client.println(".data{padding: 10px;}");
 
-client.println("</style>");
+    client.println("</style>");
 
-client.println("</head>");
+    client.println("</head>");
 
-client.println("<body>");
+    client.println("<body>");
 
-client.println("<div id=\"webpage\">");   
+    client.println("<div id=\"webpage\">");
 
-client.println("<h1>ESP32 Interfacing with DHT11</h1>");
+    client.println("<h1>ESP32 Interfacing with DHT11</h1>");
 
-client.println("<div class=\"data\">");
+    client.println("<div class=\"data\">");
 
-client.println("<div class=\"side_adjust text1\">Humidity:</div>");
+    client.println("<div class=\"side_adjust text1\">Humidity:</div>");
 
-client.println("<div class=\"side_adjust data1\">");
+    client.println("<div class=\"side_adjust data1\">");
 
-client.print(Humidity);
+    client.print(Humidity);
 
-client.println("<div class=\"side_adjust text1\">%</div>");
+    client.println("<div class=\"side_adjust text1\">%</div>");
 
-client.println("</div>");
+    client.println("</div>");
 
-client.println("<div class=\"data\">");
+    client.println("<div class=\"data\">");
 
-client.println("<div class=\"side_adjust text2\">Temperature:</div>");
+    client.println("<div class=\"side_adjust text2\">Temperature:</div>");
 
-client.println("<div class=\"side_adjust data2\">");
+    client.println("<div class=\"side_adjust data2\">");
 
-client.print(Temperature);
+    client.print(Temperature);
 
-client.println("<div class=\"side_adjust text2\">*C</div>");
+    client.println("<div class=\"side_adjust text2\">*C</div>");
 
-client.println("</div>");
+    client.println("</div>");
 
-client.println("</div>");
+    client.println("</div>");
 
-client.println("</body>");
+    client.println("</body>");
 
-client.println("</html>");
+    client.println("</html>");
 
- delay(4000);
+    delay(4000);
 
-  if ( Temperature >= 20) {
+    if (Temperature >= 20)
+    {
 
-        send_event("temp_event");
-
-        }
-
-}
-
+      send_event("temp_event");
+    }
+  }
 }
 
 void send_event(const char *event)
@@ -180,12 +175,12 @@ void send_event(const char *event)
 
   const int httpPort = 80;
 
-  if (!client.connect(host, httpPort)) {
+  if (!client.connect(host, httpPort))
+  {
 
     Serial.println("Connection failed");
 
     return;
-
   }
 
   // We now create a URI for the request
@@ -206,30 +201,29 @@ void send_event(const char *event)
 
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
 
-               "Host: " + host + "\r\n" + 
+               "Host: " + host + "\r\n" +
 
                "Connection: close\r\n\r\n");
 
-  while(client.connected())
+  while (client.connected())
 
   {
 
-    if(client.available())
+    if (client.available())
 
     {
 
       String line = client.readStringUntil('\r');
 
       Serial.print(line);
-
-    } else {
+    }
+    else
+    {
 
       // No data yet, wait a bit
 
       delay(50);
-
     };
-
   }
 
   Serial.println();
@@ -237,5 +231,4 @@ void send_event(const char *event)
   Serial.println("closing connection");
 
   client.stop();
-
 }
